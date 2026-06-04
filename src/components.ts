9a3a752b1969b381120111f651cbebb4affa6049
Blawness <ikashaafitri@gmail.com>
2026-06-04 19:15:28 +0700

    feat: export ConfirmDelete (first shippable component)

---
+++ src/components.ts
@@ -0,0 +1,14 @@
+export { ConfirmDelete } from "./components/confirm-delete";
+export { Button, buttonVariants } from "./components/ui/button";
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
